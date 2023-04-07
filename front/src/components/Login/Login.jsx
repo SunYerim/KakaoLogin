5b38c10afda3c6decef6150fbd501470d5fad8f9
Sun Yerim <syl1602@naver.com>
2023-04-07 15:17:58 +0900

    Login.jsx 수정

--- front/src/components/Login/Login.jsx
+++ front/src/components/Login/Login.jsx
@@ -1,70 +1,105 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 import S from "./styled";
-import KakaoLogin from "react-kakao-login";
+//import Kakao from "kakao.js";
 
+class Login extends Component {
+  state = {
+    isLogin: false,
+  };
+  loginWithKakao = () => {
+    try {
+      return new Promise((resolve, reject) => {
+        if (!window.Kakao) {
+          reject("Kakao 인스턴스가 존재하지 않습니다.");
+        }
+        window.Kakao.Auth.login({
+          success: (auth) => {
+            console.log("정상적으로 로그인 되었습니다.", auth);
+            this.setState({
+              isLogin: true,
+            });
+          },
+          fail: (err) => {
+            console.error(err);
+          },
+        });
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  logoutWithKakao = () => {
+    if (window.Kakao.Auth.getAccessToken()) {
+      console.log(
+        "카카오 인증 액세스 토큰이 존재합니다.",
+        window.Kakao.Auth.getAccessToken()
+      );
+      window.Kakao.Auth.logout(() => {
+        console.log("로그아웃 되었습니다", window.Kakao.Auth.getAccessToken());
+        this.setState({
+          isLogin: false,
+        });
+      });
+    }
+  };
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://developers.kakao.com/sdk/js/kakao.min.js";
+    document.head.appendChild(script);
+    script.onload = () => {
+      window.Kakao.init("5e8e12531dcf57d869bb229c445390ef");
+      if (window.Kakao.Auth.getAccessToken()) {
+        console.log("액세스 토큰이 존재합니다. 세션을 유지합니다.");
+        this.setState({
+          isLogin: true,
+        });
+      }
+    };
+  }
+  render() {
+    const { isLogin } = this.state;
+
+    const loginView = (
+      <div>
+        <p>메인 화면</p>
+        <button onClick={this.loginWithKakao}>카카오 로그인</button>
+      </div>
+    );
+
+    const mainView = (
+      <div>
+        <p>메인 화면</p>
+        <button onClick={this.logoutWithKakao}>카카오 로그아웃</button>
+      </div>
+    );
+
+    return <div className="Login">{isLogin ? mainView : loginView}</div>;
+  }
+}
+export default Login;
+
+/*
 const Login = () => {
-  const JavaScript_Key = "my_app_key";
+  const JavaScript_Key = "d19046b7f49f6376e5f701a2a57167e3";
   const REDIRECT_URI = "http://localhost:3000/oauth";
   //const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${JavaScript_Key}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
+  //Kakao.init(JavaScript_Key);
+
   function loginWithKakao() {
     if (typeof window.Kakao !== "undefined") {
-      window.Kakao.Auth.authorize({
+      Kakao.Auth.login({
+        success: function (authObj) {
+          alert("액세스 토큰: " + authObj.access_token);
+        },
+        fail: function (err) {
+          alert(JSON.stringify(err));
+        },
         redirectUri: REDIRECT_URI,
       });
     }
   }
-
-  function displayToken() {
-    const token = getCookie("authorize-access-token");
-
-    if (token) {
-      window.Kakao.Auth.setAccessToken(token);
-      window.Kakao.Auth.getStatusInfo()
-        .then(function (res) {
-          if (res.status === "connected") {
-            var accessToken = window.Kakao.Auth.getAccessToken();
-            console.log(accessToken);
-            document.getElementById("token-result").innerText =
-              "login success, token: " + accessToken;
-          }
-        })
-        .catch(function (err) {
-          console.log(err);
-          window.Kakao.Auth.setAccessToken(null);
-        });
-    }
-  }
-
-  function getCookie(name) {
-    var parts = document.cookie.split(name + "=");
-    if (parts.length === 2) {
-      return parts[1].split(";")[0];
-    }
-  }
-
-  useEffect(() => {
-    if (!window.Kakao) {
-      KakaoLogin.init(JavaScript_Key);
-      window.Kakao = KakaoLogin;
-    }
-  }, []);
-
-  useEffect(() => {
-    if (typeof window.Kakao !== "undefined") {
-      window.Kakao.Auth.createLoginButton({
-        container: "#kakao-login-btn",
-        success: function (authObj) {
-          console.log(authObj);
-          window.Kakao.Auth.setAccessToken(authObj.access_token);
-          displayToken();
-        },
-        fail: function (err) {
-          console.log(err);
-        },
-      });
-    }
-  }, []);
 
   return (
     <S.Container>
@@ -74,8 +109,8 @@ const Login = () => {
 
         <S.BtnList>
           <div>
-            {/* 카카오 로그인 버튼 */}
-            <div id="kakao-login-btn" onClick={() => loginWithKakao()}>
+            
+            <div id="kakao-login-btn" onClick={loginWithKakao}>
               <img
                 src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
                 width="222"
@@ -89,4 +124,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Login;*/
